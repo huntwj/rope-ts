330ec2c13c3913c260849a8d61d9b288f7e76766
Wil Hunt <huntwj@gmail.com>
2019-10-10 01:37:57 -0700

    - add prettier:fix script to package.json - add basic functions to rope.ts   * fromString   * isLeaf (guard)   * length   * concat   * charAt

--- src/rope.ts
+++ src/rope.ts
@@ -1,13 +1,38 @@
 export interface Node {
-  data: string;
-
   left: Rope;
   right: Rope;
 }
 
-export type Leaf = "_leaf";
+export type Leaf = string;
 
-export type Rope =
-  Leaf | Node;
+export type Rope = Leaf | Node;
 
-export const empty = (): Rope => "_leaf";
+export const empty = (): Rope => "";
+
+export const fromString = (data: string): Rope => data;
+
+export const isLeaf = (rope: Rope): rope is Leaf => typeof rope === "string";
+
+export const length = (rope: Rope): number =>
+  isLeaf(rope) ? rope.length : length(rope.left) + length(rope.right);
+
+export const concat = (left: Rope, right: Rope): Rope =>
+  left === ""
+    ? right
+    : right === ""
+    ? left
+    : {
+        left,
+        right,
+      };
+
+export const charAt = (index: number) => (rope: Rope): string => {
+  if (isLeaf(rope)) {
+    return rope[index];
+  } else {
+    const leftLen = length(rope.left);
+    return leftLen < index
+      ? charAt(index - leftLen)(rope.right)
+      : charAt(index)(rope.left);
+  }
+};
--- test/rope.ts
+++ test/rope.ts
@@ -1,11 +1,58 @@
 import * as Rope from "../src/index";
 
-describe("Placeholder rope test", () => {
+describe("Empty Ropes", () => {
   it("should create an empty rope", () => {
     expect(Rope.empty()).not.toBeUndefined();
   });
 
   it("should be consistent when empty", () => {
     expect(Rope.empty()).toEqual(Rope.empty());
+  });
+
+  it("should have length 0", () => {
+    expect(Rope.length(Rope.empty())).toBe(0);
+  });
+
+  it("empty concat empty should still be empty", () => {
+    expect(Rope.concat(Rope.empty(), Rope.empty())).toEqual(Rope.empty());
+  });
+
+  it("should return undefined for all charAt calls", () => {
+    expect(Rope.charAt(0)(Rope.empty())).toBeUndefined();
+    expect(Rope.charAt(1)(Rope.empty())).toBeUndefined();
+  });
+});
+
+describe("Short Ropes", () => {
+  const test = "test string";
+  const rope = Rope.fromString(test);
+  const test2 = "1234";
+  const rope2 = Rope.fromString(test2);
+
+  it("should be able to create a rope from a string", () => {
+    expect(rope).not.toEqual(Rope.empty());
+    expect(Rope.length(rope)).toBe(test.length);
+  });
+
+  describe("concatenation", () => {
+    describe("empty ropes", () => {
+      it("should be left idempotent", () => {
+        expect(Rope.length(Rope.concat(Rope.empty(), test))).toBe(
+          Rope.length(rope),
+        );
+      });
+      it("should be right idempotent", () => {
+        expect(Rope.length(Rope.concat(test, Rope.empty()))).toBe(
+          Rope.length(rope),
+        );
+      });
+      it("should concat two non-empty ropes", () => {
+        const big = Rope.concat(rope, rope2);
+        expect(Rope.length(big)).toBe(Rope.length(rope) + Rope.length(rope2));
+
+        expect(Rope.charAt(5)(big)).toBe("s");
+        expect(Rope.charAt(12)(big)).toBe("2");
+      });
+    });
   });
 });
